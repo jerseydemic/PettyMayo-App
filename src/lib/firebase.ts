aa7f9cfce26c6091da78ad153898aa8a1d50c82f
jerseydemic <jerseydemic@gmail.com>
2026-01-17 11:53:48 -0500

    fix(firebase): disable persistence to prevent tab locking hangs

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -23,12 +23,12 @@ export const analytics = getAnalytics(app);
 
 import { enableIndexedDbPersistence } from 'firebase/firestore';
 
-enableIndexedDbPersistence(db).catch((err) => {
-    if (err.code == 'failed-precondition') {
-        // Multiple tabs open, persistence can only be enabled in one tab at a a time.
-        console.warn('Firestore persistence failed: Multiple tabs open');
-    } else if (err.code == 'unimplemented') {
-        // The current browser does not support all of the features required to enable persistence
-        console.warn('Firestore persistence failed: Browser not supported');
-    }
-});
+// enableIndexedDbPersistence(db).catch((err) => {
+//     if (err.code == 'failed-precondition') {
+//         // Multiple tabs open, persistence can only be enabled in one tab at a a time.
+//         console.warn('Firestore persistence failed: Multiple tabs open');
+//     } else if (err.code == 'unimplemented') {
+//         // The current browser does not support all of the features required to enable persistence
+//         console.warn('Firestore persistence failed: Browser not supported');
+//     }
+// });
